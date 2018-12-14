d8da23b418ce3be8506072a4af67c9eef26aea94
Claudiofs <44906473+Claudiofs@users.noreply.github.com>
2018-12-14 16:46:46 -0200

    Adicionei saidaDados

---
+++ saidaDados.py
@@ -0,0 +1,47 @@
+
+# Variáveis
+
+
+
+
+
+Digite endereço empresa"))
+print(endereçoAmigo)
+
+
+# Declaração de Variáveis
+
+
+
+
+# Entrada de dados
+nomeAmigo = string(input("Digite nome amigo"))
+print(nomeAmigo)
+
+endereçoAmigo = string(input("Digite endereço amigo"))
+print(endereçoAmigo)
+
+telefoneAmigo = string(input("Digite telefone amigo"))
+print(telefoneAmigo)
+
+celularAmigo = string(input("Digite celular amigo"))
+print(celularAmigo)
+
+nomeEmpresa = string(input("Digite nome da empresa"))
+print(nomeEmpresa)
+
+telefoneEmpresa = string(input("Digite telefone empresa"))
+print(telefoneAmigo)
+
+endereçoEmprea = string(input("
+
+# Processamento
+
+                              
+                              
+                              
+                              
+
+# saída de dados
+
+print(""%resultado)
