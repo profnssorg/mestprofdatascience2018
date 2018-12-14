94198c8352c62035bb5e3ebb6fc2de7595e64e72
Valmor Mantelli Jr <44906350+Valmorj@users.noreply.github.com>
2018-12-14 16:40:44 -0200

    Adicionei entrada de dados

---
+++ entradaDados.py
@@ -0,0 +1,7 @@
+#Entrada das variáveis
+nome = input("Digite o nome: ")
+endereço = input("Digite o endereço: ")
+telefone = input("Digite o número de telefone: ")
+celular = input("Digite o número de celular: ")
+empresa = input("Digite o nome da empresa: ")
+enderecoEmpresa = input("Digite o endereço da empresa: ")
